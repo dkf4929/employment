b55a540c523fdcf17ff9bbc53e7b11b6da8ef1fc
yeong-ho <dkf4928@naver.com>
2023-04-19 21:26:21 +0900

    대학교 검색 API 개발

--- src/test/java/com/project/employment/EmploymentApplicationTests.java
+++ src/test/java/com/project/employment/EmploymentApplicationTests.java
@@ -8,6 +8,10 @@ class EmploymentApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String s = "20200201";
+		System.out.println("s1 = " + s.substring(0,4));
+		System.out.println("s2 = " + s.substring(4,6));
+		System.out.println("s3 = " + s.substring(6,8));
 	}
 
 }
