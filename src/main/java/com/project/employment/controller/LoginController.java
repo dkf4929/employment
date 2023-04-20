d5d5089e020644b6a510aeb6266734e7725860ed
dkf4929 <sng4929@naver.com>
2023-04-20 22:34:07 +0900

    로그인 처리

--- src/main/java/com/project/employment/controller/LoginController.java
+++ src/main/java/com/project/employment/controller/LoginController.java
@@ -1,7 +1,22 @@
 package com.project.employment.controller;
 
+import com.project.employment.dto.LoginDto;
+import com.project.employment.service.LoginService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/login")
+@RequiredArgsConstructor
 public class LoginController {
+    private final LoginService loginService;
+
+    @ResponseBody
+    @PostMapping
+    public String login(LoginDto dto) {
+        return loginService.login(dto);
+    }
 }
---
+++ src/main/java/com/project/employment/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.project.employment.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String loginId;
+    private String password;
+}
