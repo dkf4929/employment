597ceec8e7ac595190218c462fe47195e626c042
dkf4929 <sng4929@naver.com>
2023-04-20 22:33:09 +0900

    로그인 시 exception 처리

--- src/main/java/com/project/employment/controller/ExceptionController.java
+++ src/main/java/com/project/employment/controller/ExceptionController.java
@@ -1,20 +1,26 @@
 package com.project.employment.controller;
 
 import com.project.employment.exception.ConfirmPasswordMismatchException;
+import com.project.employment.exception.NoSuchUserException;
+import com.project.employment.exception.PasswordMismatchException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionController {
 
     @ExceptionHandler
     public ResponseEntity<String> handleException(Exception e) {
-        if (e instanceof ConfirmPasswordMismatchException) {
+        log.error("exception", e);
+
+        if (e instanceof ConfirmPasswordMismatchException || e instanceof PasswordMismatchException || e instanceof NoSuchUserException) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
-        return null;
+        return new ResponseEntity<>("서버 오류 발생!!", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
