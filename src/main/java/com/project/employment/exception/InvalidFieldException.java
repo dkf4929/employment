4e5ac81a98e810bad23c6b314ba39a7de0c232cb
dkf4929 <sng4929@naver.com>
2023-04-21 20:38:21 +0900

    exception 처리

---
+++ src/main/java/com/project/employment/exception/InvalidFieldException.java
@@ -0,0 +1,19 @@
+package com.project.employment.exception;
+
+public class InvalidFieldException extends CommonException {
+    public InvalidFieldException() {
+        super();
+    }
+
+    public InvalidFieldException(String message) {
+        super(message);
+    }
+
+    public InvalidFieldException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidFieldException(Throwable cause) {
+        super(cause);
+    }
+}
