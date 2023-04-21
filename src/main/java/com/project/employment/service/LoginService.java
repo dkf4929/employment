4213811a5d122bde04346218595682a636a00fc3
dkf4929 <sng4929@naver.com>
2023-04-21 20:42:44 +0900

    로그인 기능 수정

--- src/main/java/com/project/employment/service/LoginService.java
+++ src/main/java/com/project/employment/service/LoginService.java
@@ -11,12 +11,14 @@ import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class LoginService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
