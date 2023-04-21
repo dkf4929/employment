5fd110f532f30528633b8928efdd1ae41af93b06
yeong-ho <dkf4928@naver.com>
2023-04-21 20:39:48 +0900

    구글 로그인 시 회원정보 수정 기능 개발

--- src/main/java/com/project/employment/controller/MemberController.java
+++ src/main/java/com/project/employment/controller/MemberController.java
@@ -1,12 +1,19 @@
 package com.project.employment.controller;
 
 import com.project.employment.dto.MemberSaveDto;
+import com.project.employment.entity.Member;
 import com.project.employment.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +42,16 @@ public class MemberController {
     }
 
     @PostMapping("/add")
-    public void save(@Valid MemberSaveDto dto) {
-        memberService.save(dto);
+    public void save(@Valid MemberSaveDto dto, BindingResult bindingResult) {
+        memberService.save(dto, bindingResult);
+    }
+
+    @GetMapping("/edit")
+    public String edit(Model model) {
+        Member loginMember = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("email", loginMember.getEmail());
+        model.addAttribute("memberName", loginMember.getMemberName());
+
+        return "member/edit";
     }
 }
