d7a64666e913ff7fbe98229bd9d5de74ed50512b
dkf4929 <sng4929@naver.com>
2023-04-21 20:43:19 +0900

    필드 에러 시 exception 처리

--- src/main/java/com/project/employment/service/MemberService.java
+++ src/main/java/com/project/employment/service/MemberService.java
@@ -3,14 +3,18 @@ package com.project.employment.service;
 import com.project.employment.dto.MemberSaveDto;
 import com.project.employment.entity.Member;
 import com.project.employment.exception.ConfirmPasswordMismatchException;
+import com.project.employment.exception.InvalidFieldException;
 import com.project.employment.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -18,7 +22,11 @@ public class MemberService {
         return new ResponseEntity<>(memberRepository.countByLoginId(loginId), HttpStatus.OK);
     }
 
-    public void save(MemberSaveDto dto) {
+    public void save(MemberSaveDto dto, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            throw new InvalidFieldException(bindingResult.getFieldError().getDefaultMessage());
+        }
+
         if (!dto.getPassword().equals(dto.getConfirmPassword())) {
             throw new ConfirmPasswordMismatchException("비밀번호가 일치하지 않습니다.");
         }
