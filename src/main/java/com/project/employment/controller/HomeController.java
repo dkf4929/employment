c6329e20b4e27e0a74c8c0d41327ab72541d1436
yeong-ho <dkf4928@naver.com>
2023-04-22 19:06:49 +0900

    홈 화면 추가

--- src/main/java/com/project/employment/controller/HomeController.java
+++ src/main/java/com/project/employment/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.project.employment.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping("/")
     public String loginPage() {
         return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        return "home";
     }
 }
