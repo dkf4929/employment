87593c930adae4ab8ef917d84b82d690f95ad224
yeong-ho <dkf4928@naver.com>
2023-05-01 20:44:47 +0900

    파일 저장 기능 추가

---
+++ src/main/java/com/project/employment/common/WhitePath.java
@@ -0,0 +1,5 @@
+package com.project.employment.common;
+
+public class WhitePath {
+    public static final String[] WHITE_LIST = {"/member/add", "/resources/**", "/", "/api/**", "/image/**", "/login", "/member/id_check", "/member/school-search", "/upload/**"};
+}
