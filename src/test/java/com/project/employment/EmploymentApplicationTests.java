337772c23ecaf7d85758d3b5e1e27f945da43f66
dkf4929 <sng4929@naver.com>
2023-05-05 22:40:00 +0900

    불필요한 코드 제거

--- src/test/java/com/project/employment/EmploymentApplicationTests.java
+++ src/test/java/com/project/employment/EmploymentApplicationTests.java
@@ -3,6 +3,8 @@ package com.project.employment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.regex.Pattern;
+
 @SpringBootTest
 class EmploymentApplicationTests {
 
