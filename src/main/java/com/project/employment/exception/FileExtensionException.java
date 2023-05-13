004d8f0261e5f29e4d95b0dc5c7947d413f1e199
dkf4929 <sng4929@naver.com>
2023-05-13 19:48:35 +0900

    refactor : 파일업로드 리팩토링

---
+++ src/main/java/com/project/employment/exception/FileExtensionException.java
@@ -0,0 +1,19 @@
+package com.project.employment.exception;
+
+public class FileExtensionException extends CommonException {
+    public FileExtensionException() {
+        super();
+    }
+
+    public FileExtensionException(String message) {
+        super(message);
+    }
+
+    public FileExtensionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileExtensionException(Throwable cause) {
+        super(cause);
+    }
+}
