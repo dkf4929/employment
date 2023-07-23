83134bb2e78fd6b2e4dc6f9cfa31669a47690175
yeonghoS <dkf4928@naver.com>
2023-07-23 16:43:58 +0900

    chore: 예외처리

---
+++ src/main/java/com/project/employment/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.project.employment.exception;
+
+public class BusinessException extends CommonException {
+    public BusinessException() {
+        super();
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+}
