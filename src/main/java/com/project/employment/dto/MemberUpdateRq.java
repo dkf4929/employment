923c7b02ad93b015ce0e49af4c18089e800ab1e4
dkf4929 <sng4929@naver.com>
2023-07-23 17:42:20 +0900

    chore: 필드 validation 처리

--- src/main/java/com/project/employment/dto/MemberUpdateRq.java
+++ src/main/java/com/project/employment/dto/MemberUpdateRq.java
@@ -3,32 +3,34 @@ package com.project.employment.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
+@Getter
+@Setter
 public class MemberUpdateRq {
     private Long memberId;
 
-    @NotBlank
+    @NotBlank(message = "이름을 입력하세요.")
     @Length(min = 2, max = 50)
     @Pattern(regexp = "^[a-zA-Z\\s]+$|^[가-힣\\s]+$", message = "성명은 한글 또는 영어만 입력 가능합니다.")
     private String memberName;
 
-    @NotBlank
+    @NotBlank(message = "생일을 입력하세요.")
     private String birthday;
 
-    @NotBlank
+    @NotBlank(message = "이메일 주소를 입력하세요")
     @Email(message = "올바른 이메일 주소를 입력하세요.")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "휴대폰번호를 입력하세요.")
     @Pattern(regexp="^01([016789])?[1-9]\\d{2,3}\\d{4}$", message="휴대폰 번호 형식에 맞게 입력해주세요.")
     private String phoneNumber;
 
     private MultipartFile file;
 
-    @NotBlank
+    @NotBlank(message = "최종학력을 입력하세요.")
     private String schoolName;
 }
