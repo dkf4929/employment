22640029587ed56d47e4d5286c036f3910003b84
dkf4929 <sng4929@naver.com>
2023-10-01 18:47:56 +0900

    refactor: 파일업로드 리팩토링

--- src/main/java/com/project/employment/enums/AttachType.java
+++ src/main/java/com/project/employment/enums/AttachType.java
@@ -1,16 +1,6 @@
 package com.project.employment.enums;
 
 public enum AttachType {
-    IMAGE("IMAGE"), // 이미지 파일
-    DOCUMENT("DOCUMENT"); // 이력서, 경력기술서, 자격증 등
-
-    private String document;
-
-    AttachType(String document) {
-        this.document = document;
-    }
-
-    public String getDocument() {
-        return document;
-    }
+    IMAGE,
+    DOCUMENT;
 }
