0027a9e6adefccd3d05ae745c452ee7236c05483
yeonghoS <dkf4928@naver.com>
2023-10-01 18:48:21 +0900

    flyway: 컬럼 수정

--- src/main/resources/db/V0.0.3__attachFile.sql
+++ src/main/resources/db/V0.0.3__attachFile.sql
@@ -2,7 +2,7 @@ create table attach_file (
     attach_file_id bigint not null auto_increment,
     attach_entity varchar(255),
     attach_type varchar(255),
-    entity bigint,
+    entity_id bigint,
     file_name varchar(255),
     file_path varchar(255),
 primary key (attach_file_id)) engine=InnoDB
