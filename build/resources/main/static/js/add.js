21e958b60f19e22b60fbd5539f3109d9351c0a1e
dkf4929 <sng4929@naver.com>
2023-10-08 18:26:36 +0900

    feat: 첨부파일 등록 팝업 function 추가

--- build/resources/main/static/js/add.js
+++ build/resources/main/static/js/add.js
@@ -100,15 +100,6 @@ myForm.addEventListener('submit', event => {
     });
 });
 
-function imageUploadPopup() {
-    const w = 650;
-    const h = 330;
-    const left = screen.width / 2 - w / 2;
-    const top = screen.height / 2 - h / 2;
-
-    window.open("../attach?attachType=IMAGE&attachEntity=MEMBER", "_blank", `width=${w}, height=${h}, top=${top}, left=${left}`);
-}
-
 function getUploadResponse(data) {
     var data = JSON.parse(data);
     document.getElementById('imageFileName').value = data.fileName;
@@ -116,21 +107,5 @@ function getUploadResponse(data) {
     document.getElementById('filePath').value = data.filePath;
 
     previewImage(data.filePath);
-}
-
-function previewImage(filePath) {
-    var previewImage = document.getElementById('previewImage');
-    var noImage = document.getElementById('noImage');
-
-    if (filePath) {
-        // 이미지 파일이 있는 경우
-        previewImage.src = "/attach/view?filePath=" + filePath;
-        previewImage.style.display = 'block';
-        noImage.style.display = 'none';
-    } else {
-        // 이미지 파일이 없는 경우
-        previewImage.style.display = 'none';
-        noImage.style.display = 'block';
-    }
 }
 
